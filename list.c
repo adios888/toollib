2d56efa588276c701fd1f20411b90ac5a30de75d
jun.zhao <jun.zhao@panderminer.com>
2018-05-10 15:30:36 +0800

    remove main func in list file

--- list.c
+++ list.c
@@ -1,15 +1,4 @@
-struct list_head
-{
-    struct list_head *next,*prev;
-};
 
-#define LIST_HEAD_INIT(name) {&(name),&(name)}
-#define LIST_HEAD(name) \
-    struct list_head name = LIST_HEAD_INIT(name)
-
-#define INIT_LIST_HEAD(ptr) do{ \
-    (ptr)->next = (ptr); (ptr)->prev = (ptr); \
-}while(0)
 
 
 static inline void __list_add(struct list_head *new,
@@ -28,33 +17,7 @@ static inline void list_add(struct list_head *new, struct list_head *head)
 }
 
 
-int main()
-{
-    struct list_head test =
-    {
-        &test,
-        &test
-    };    
 
-    struct zero 
-    {
-        int a;
-        char b;
-        char *p;
-    }ztest;
-
-    LIST_HEAD(t1);
-
-    struct list_head *head;
-    INIT_LIST_HEAD(head);
-    
-    void *p = ((struct list_head*)0)->prev;
-
-    void *p1 = ((struct zero*)0)->p;
-
-
-    return 0;
-}
 
 
 
